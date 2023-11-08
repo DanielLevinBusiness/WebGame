527fd087a79cf7a300755c57337a1c1d6797d353
DanielLevinBusiness <74470055+DanielLevinBusiness@users.noreply.github.com>
2023-11-08 12:37:45 +0200

    Add files via upload

---
+++ dani-daniel-branch/README.md
@@ -0,0 +1,27 @@
+# Dani & Sofi = <3
+
+## Todo checklist
+
+* Metodology and team work
+* ide\ workspace needed tools & keymap,extentions vscode.
+* dependencies package manager high level
+* design - uml and high level
+* Missions and responsibilties.
+* project name and story.
+* enviroment and deploymnet.
+
+## Metodology and team work
+
+* main is the main branch
+* when you are working on feature or bug you are need to open a new branch.
+* after someone have done his feature \ task he need PR and merge it to develop.
+after some tests we can merge into main. and create an release on something.
+
+## ide\ workspace needed tools
+
+* every one can choose his ide but i"m think its the best to use vscode right now.
+* npm - installed with node.js .
+* pixi package.
+* key bind and key map.
+* vscode - CTRL + SHIFT + P
+* ALT+ ENTER
---
+++ dani-daniel-branch/game.js
@@ -0,0 +1,147 @@
+let app = new PIXI.Application({
+  //Create a Pixi Application
+  width: 640,
+  height: 640,
+  antialias: true,
+  transparent: true,
+  resolution: 1,
+});
+
+document.body.appendChild(app.view); //Add the canvas that Pixi automatically created for you to the HTML document
+
+var gravity = 1;
+var xVelocity = 0;
+var yVelocity = 0;
+var jumping = true;
+
+PIXI.loader.add("player", "pictures/characters.png").add("background", "pictures/sheet.png").load(setup); //load an image and run the `setup` function when it's done
+
+function setup() {
+  let playerTileSet = PIXI.utils.TextureCache["player"]; //sets the tileSet
+
+  drawBackground();
+
+
+  let playerRectangle = new PIXI.Rectangle(0, 32, 32, 32); //sets the player frame inside the tileSet, x, y, width of ractangle, height of ractangle
+
+  playerTileSet.frame = playerRectangle; //Tell the texture to use that rectangular section
+
+  let player = new PIXI.Sprite(playerTileSet); //Create the sprite from the texture
+
+  app.renderer.render(app.stage); //Render the stage
+  
+  app.stage.addChild(player); //Add the player to the stage
+
+
+  player.anchor.x = 0.5; //sets the anchor of the picture to the middle
+  player.anchor.y = 0.5;
+
+  player.y = app.stage.height / 2; //sets the starting positin
+  player.x = 100;
+
+
+  function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    document.addEventListener("keydown", onKeyDown);
+    outOfBounds();
+    psudoPhysics();
+  }
+
+  gameLoop();
+
+  function drawBackground(){
+
+    let backgroundTileSet = PIXI.utils.TextureCache["background"]; //sets the tileSet
+    
+    const map =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 46, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 46, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 46, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 46, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 21, 22, 23, 24, 0, 0, 45, 46, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 37, 38, 45, 46, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 46, 47, 54, 55, 56, 57, 58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 71, 72, 73, 74, 75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 46, 47, 0, 0, 0, 0, 88, 89, 90, 91, 92, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 105, 106, 107, 108, 109, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 119, 117, 117, 117, 117, 117, 85, 16, 16, 16, 16, 16, 0, 0, 122, 123, 124, 125, 126, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29];
+        
+
+    for (let index = map.length - 1; index > -1; -- index) {
+      
+
+      let value = map[index];
+      let source_x =           (value % 8) * 16;
+      let source_y = Math.floor(value / 17) * 16;
+      let destination_x =           (index % 40) * 16;
+      let destination_y = Math.floor(index / 40) * 16;
+
+    let backgroundRectangle = new PIXI.Rectangle(source_x, source_y, 16, 16); //sets the map frame inside the tileSet, x, y, width of ractangle, height of ractangle
+    backgroundTileSet.frame = backgroundRectangle;
+    let background = new PIXI.Sprite(backgroundTileSet);
+    background.y = destination_y;
+    background.x = destination_x;
+    app.stage.addChild(background);//keep background before player, so player wont dissapear
+
+    }
+    
+  }
+
+
+
+  function psudoPhysics() {
+    yVelocity += gravity; // gravity
+    player.x += xVelocity;
+    player.y += yVelocity;
+    if (!jumping)//to be able to move in mid air
+      xVelocity *= 0.9; // friction
+    yVelocity *= 0.9; // friction
+  }
+
+  function outOfBounds() {
+
+    if (player.y > 380 && player.x > 60 && player.x < 150) {
+      jumping = false; //to prevent double jump
+      player.y = 380;
+    }
+
+    if (player.y > 640){
+      player.y = app.stage.height / 2; //sets the starting positin
+      player.x = 100;
+    }
+
+    if (player.y < 32) {
+      //stops player on the "roof"
+      yVelocity = 0;
+      player.y = 32;
+    }
+
+    if (player.x < 8) {
+      //stop on the left side, player size is 32 pix
+      xVelocity = 0; //so the player doesnt "vibrate"
+      player.x = 8;
+    }
+
+    if (player.x > 632) {
+      //stop on the right side, player size is 32 pix
+      xVelocity = 0; //so the player doesnt "vibrate"
+      player.x = 632;
+    }
+  }
+
+  function onKeyDown(key) {
+    // W Key is 87
+    // Up arrow is 87
+    if (!jumping && (key.keyCode === 87 || key.keyCode === 38)) {
+      yVelocity -= 20;
+      jumping = true;
+    }
+
+    // S Key is 83
+    // Down arrow is 40
+    // if (key.keyCode === 83 || key.keyCode === 40) {//no reason to go down
+    //   yVelocity += 5;
+    // }
+
+    // A Key is 65
+    // Left arrow is 37
+    if (key.keyCode === 65 || key.keyCode === 37) {
+      xVelocity -= 1;
+    }
+
+    // D Key is 68
+    // Right arrow is 39
+    if (key.keyCode === 68 || key.keyCode === 39) {
+      xVelocity += 1;
+    }
+  }
+}
